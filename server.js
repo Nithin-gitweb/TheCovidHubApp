555701947c435dd1654cc7a0976864b9a1e8bd24
Nitin TPN <npt1998@outlook.com>
2021-03-02 11:18:45 +0530

    Update 400

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/the-covid-hub-app'));
+app.use(express.static(__dirname + './dist/the-covid-hub-app'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/the-covid-hub-app/index.html'));
+res.sendFile(path.join(__dirname+'./dist/the-covid-hub-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
